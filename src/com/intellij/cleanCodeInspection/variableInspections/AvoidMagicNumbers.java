b2e1297ac377e9b9000ca773112b01eba1a8c44b
nkanakis <nicolas.kanakis@gmail.com>
2017-09-19 17:19:10 +0300

    todo inspections

---
+++ src/com/intellij/cleanCodeInspection/variableInspections/AvoidMagicNumbers.java
@@ -0,0 +1,11 @@
+package com.intellij.cleanCodeInspection.variableInspections;
+
+import com.intellij.codeInspection.BaseJavaLocalInspectionTool;
+
+/**
+ * Created by nkanakis on 9/19/2017.
+ */
+public class AvoidMagicNumbers extends BaseJavaLocalInspectionTool {
+
+    //todo search and mark magic and mark them.
+}
---
+++ src/com/intellij/cleanCodeInspection/variableInspections/ProblematicVariableNames.java
@@ -0,0 +1,11 @@
+package com.intellij.cleanCodeInspection.variableInspections;
+
+import com.intellij.codeInspection.BaseJavaLocalInspectionTool;
+
+/**
+ * Created by nkanakis on 9/19/2017.
+ */
+public class ProblematicVariableNames extends BaseJavaLocalInspectionTool {
+
+    //todo search for variables  O & l as well as variables containing String, Double, Integer, Long etc...
+}
